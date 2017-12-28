ceed52a7244dab3a15964a663199a4eeebad4b2f
Vignesh A <vignesha@ilink-systems.com>
2017-12-28 17:45:35 +0530

    Cortona event listener method updated

--- PWACurrencyConverterHostedWebApp/js/app.js
+++ PWACurrencyConverterHostedWebApp/js/app.js
@@ -195,40 +195,41 @@ function showTileInWindows10 () {
 
 function CortanaVoiceServices ()
 {
-	alert("cortana here");
-	//if (typeof Windows !== 'undefined') {
-  //console.log('Windows namespace is available');
-  // Subscribe to the Windows Activation Event
-  //Windows.UI.WebUI.WebUIApplication.addEventListener('activated', function (args) {
-    //var activation = Windows.ApplicationModel.Activation;
-    // Check to see if the app was activated by a voice command
-    //if (args.kind === activation.ActivationKind.voiceCommand) {
+	if (typeof Windows !== 'undefined') {
+	  //console.log('Windows namespace is available');
+	  ('Windows namespace is available');
+	  // Subscribe to the Windows Activation Event
+	  Windows.UI.WebUI.WebUIApplication.addEventListener('activated', function (args) {
+	    var activation = Windows.ApplicationModel.Activation;
+	    // Check to see if the app was activated by a voice command
+	    if (args.kind === activation.ActivationKind.voiceCommand) {
 
-      //var speechRecognitionResult = args.result;
-      //var textSpoken = speechRecognitionResult.text;
-      //var command = speechRecognitionResult.rulePath[0];
+	      var speechRecognitionResult = args.result;
+	      var textSpoken = speechRecognitionResult.text;
+	      var command = speechRecognitionResult.rulePath[0];
 
-      //console.log('The command is: ' + command);
-      //document.getElementById('command').innerHTML = command;
-      //document.getElementById('speechReco').innerHTML = speechRecognitionResult;
-      //document.getElementById('text').innerHTML = textSpoken;
+	      //console.log('The command is: ' + command);
+	      alert('The command is: ' + command);
+	      /*document.getElementById('command').innerHTML = command;
+	      document.getElementById('speechReco').innerHTML = speechRecognitionResult;
+	      document.getElementById('text').innerHTML = textSpoken;
 
-      // Determine the command type {play} defined in vcd
-      if (command === 'Open_Currency_Converter') {
-		  alert("opened currency converter");
-        // Determine the stream name specified
-        //console.log('The speech reco result is: ' + speechRecognitionResult);
-        //console.log('The text spoken is: ' + textSpoken);
-      }
-      else { 
-        // No valid command specified
-        console.log('No valid command specified');
-      }
-    }
-  });
-} else {
-  console.log('Windows namespace is unavaiable');
-}
+	      // Determine the command type {play} defined in vcd
+	      if (command === 'send') {
+	        // Determine the stream name specified
+	        console.log('The speech reco result is: ' + speechRecognitionResult);
+	        console.log('The text spoken is: ' + textSpoken);
+	      }
+	      else { 
+	        // No valid command specified
+	        console.log('No valid command specified');
+	      }*/
+	    }
+	  });
+	} else {
+	  //console.log('Windows namespace is unavaiable');
+	  alert('Windows namespace is unavaiable');
+	}
 }
 
 app.from.innerHTML = app.fromCurrencyName;
