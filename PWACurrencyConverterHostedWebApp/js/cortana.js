0e7d4a8d4b505123896b2b2203400b2db620d478
Vignesh A <vignesha@ilink-systems.com>
2018-01-03 16:20:11 +0530

    alert messages included in cortana.js

--- PWACurrencyConverterHostedWebApp/js/cortana.js
+++ PWACurrencyConverterHostedWebApp/js/cortana.js
@@ -4,9 +4,11 @@
 // parse out the command type and call the respective game APIs
 
 if (typeof Windows !== 'undefined') {
+    alert("Speech recognition 1");
   console.log('Windows namespace is available');
   // Subscribe to the Windows Activation Event
   Windows.UI.WebUI.WebUIApplication.addEventListener('activated', function (args) {
+    alert("Speech recognition 2");
     var activation = Windows.ApplicationModel.Activation;
     // Check to see if the app was activated by a voice command
     if (args.kind === activation.ActivationKind.voiceCommand) {
@@ -22,16 +24,19 @@ if (typeof Windows !== 'undefined') {
 
       // Determine the command type {play} defined in vcd
       if (command === 'Open') {
+        alert("Speech recognition 3");
         // Determine the stream name specified
         console.log('The speech reco result is: ' + speechRecognitionResult);
         console.log('The text spoken is: ' + textSpoken);
       }
       else { 
         // No valid command specified
+        alert("Speech recognition 4");
         console.log('No valid command specified');
       }
     }
   });
 } else {
+    alert("Speech recognition 5");
   console.log('Windows namespace is unavaiable');
 }
